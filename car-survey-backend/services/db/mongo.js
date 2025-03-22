3ceb3d022c3e2af7733bf1f5e6f24786047a3523
workdeepika <deepika.survetrics@gmail.com>
2025-03-22 08:05:34 +0530

    mongo

--- car-survey-backend/services/db/mongo.js
+++ car-survey-backend/services/db/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require("dotenv").config(); // Load environment variables
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect("mongodb+srv://iwintuDatabase:Harekrishna@iwintu.fehuq.mongodb.net/shop-backend")
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Error connecting to MongoDB', err));;
     console.log('MongoDB connected');
